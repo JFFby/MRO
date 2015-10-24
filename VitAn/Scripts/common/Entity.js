468539351110590f11b1daf090f07b588f721c60
JFF <justforfunby@gmail.com>
2015-10-24 12:53:30 +0300

    segmentation result object

---
+++ VitAn/Scripts/common/Entity.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    function findMaxLeftPx() {
+        //need to implement;
+        // find in this.Pixels
+        return this.Number * 3;
+    }
+
+    function findMaxRightPx() {
+        //need to implement;
+    }
+
+    function findMaxTopPx() {
+        //need to implement;
+    }
+
+    function findMaxBottomPx() {
+        //need to implement;
+        return 3;
+    }
+
+  var imgEntity = function (number, color, pixels) {
+
+        this.Number = number;
+        this.Color = color;
+        this.Pixels = pixels; // Array
+        this.MaxLeftPx = findMaxLeftPx.apply(this);
+        this.MaxRightPx = findMaxRightPx.apply(this);
+        this.MaxTopPx = findMaxTopPx.apply(this);
+        this.MaxBorromPx = findMaxBottomPx.apply(this);
+  }
+
+  this.imgEntity = imgEntity;
+})()
