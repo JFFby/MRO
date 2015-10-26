8459ad2b75e3801396e01a80333232463dc32927
JFF <justforfunby@gmail.com>
2015-10-26 23:21:26 +0300

    завелось на костылях

--- Node.js/router.js
+++ Node.js/router.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
-router.use(function timeLog(req, res, next) {
- console.log('Time: ', Date.now());
-	console.log(req._parsedUrl);
- next();
-});
+// router.use(function timeLog(req, res, next) {
+ // next();
+// });
+
 router.get('/a', function(req, res) {
  res.sendFile(__dirname + '/views/bug_a.html');
 });
 
-router.all('/push/:name', function(req, res) {
-	console.log(req.params);
-	console.log(req._parsedUrl);
-	res.end();
+router.get('/public/*', function(req, res) {
+ res.sendFile(__dirname + req._parsedUrl.path);
+});
+
+router.post('/push/:name', function(req, res) {	
+    var form = new formidable.IncomingForm();
+	form.parse(req, function(err,fields, files){
+		console.log(fields);
+		console.log(files);
+		res.write(JSON.stringify({success:true}));
+		res.end();
+	});
 });
 
 module.exports = router;
