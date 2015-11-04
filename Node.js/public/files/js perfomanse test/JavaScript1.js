1362c94bd3b3a653876d99d196341661db0f43e5
JFF <justforfunby@gmail.com>
2015-11-04 23:04:38 +0300

    perfomance test  (js vs c#)
    js so slowly...

---
+++ Node.js/public/files/js perfomanse test/JavaScript1.js
@@ -0,0 +1,11 @@
+﻿var start = new Date();
+var array = [];
+for (var i = 0; i < 1000000; i++) {
+    array[i] = Math.random(Math.round(Math.random() * 1000000));
+}
+
+_.sortBy(array);
+var end = new Date();
+console.log(end - start);
+
+//result 3358 ms
---
+++ Node.js/public/files/js perfomanse test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace perfomanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var random = new Random();
+            var array = new int[100000];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1000);
+            }
+            Array.Sort(array);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
+
+//result 8 ms
